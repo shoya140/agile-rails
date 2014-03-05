8909697fdca7b8967cfd39fe79dd67818385abe4
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2014-03-05 15:57:00 +0100

    shopping cart

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,4 +1,17 @@
 class Product < ActiveRecord::Base
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_item
+
+  private
+  def ensure_not_referenced_by_any_item
+    if line_items.empty?
+      return true
+    end
+    errors.add(:base, '品目が存在します')
+    return false
+  end
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
